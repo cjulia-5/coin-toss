e9638a31b5aaa2b6e24196a3a7756c81edfc02a0
cjulia-5 <jpcannon@iastate.edu>
2025-04-03 15:30:38 -0500

    updates done in class off of both of the professors uploads to github. as of right now, it doesnt work.

--- js/logic.js
+++ js/logic.js
@@ -1,95 +1,57 @@
-// function flipCoin(x) {
-//     let pick = x; // pick is set to the passed-in parameter (from html)
-//     let choice = document.getElementById('choice'); // to show player choice
-//     let flip = document.getElementById('flip'); // to show flip result
-//     let result = document.getElementById('result'); // to show game result
+let gamesPlayed = 0;
+let gamesWon= 0;
+let gamesLost= 0;
 
-//     // Determines the face of the toss
-//     let randNum = Math.ceil(Math.random() * 2); // randomly a 1 or 2
-//     let toss; // to hold the toss result
-
-//     if (randNum === 1) {
-//         toss = 'heads';
-//     } else {
-//         toss = 'tails';
-//     }
-
-//     // determines the result of guess
-//     let status; // to hold the result of the guess
-//     if (pick === toss ) {
-//         status = "won";
-//     } else {
-//         status = 'lost';
-//     }
-
-//     // read-outs of all the results
-//     choice.innerText = pick;
-//     flip.innerText = toss;
-//     result.innerText = status;
-// }
-
-// // Shows how to pass a paremeter
-// function flipCoin(pick) {
-//     let choice = document.getElementById('choice');
-//     choice.innerText = pick;
-// }
-
-// function flipCoin() {
-//     let display = document.getElementById('flip');
-//     let randNum = Math.floor(Math.random() * 2) + 1;
-//     let toss;
-//     if (randNum == 1) {
-//         toss = 'tails';
-//     } else {
-//         toss = 'heads';
-//     }
-//     display.innerText = toss;
-//     console.log(randNum);
-// }
-
-// function flipCoin() {
-//     let display = document.getElementById('flip');
-//     let randNum = Math.floor(Math.random() * 2) + 1;
-//     display.innerText = randNum;
-//     console.log(randNum);
-// }
-
-// function flipCoin() {
-//     let display = document.getElementById('flip');
-//     // let randNum = Math.ceil(Math.random() * 4); // --> 1 to 4
-//     // let randNum = Math.ceil(Math.random() * 2); // --> 1 or 2
-//     // let randNum = Math.floor(Math.random() * 2); // --> 0 or 1
-//     // let randNum = Math.ceil(Math.random()); // rounds up to integer
-//     // let randNum = Math.floor(Math.random()); // rounds down to integer
-//     let randNum = Math.random(); // generates digits between 0 and 1
-//     display.innerText = randNum;
-    
-//     // logs randNum in the console (browser inspector)
-//     console.log(randNum);
-// }
+let displayWon = document.getElementById('won');
+let displayLost = document.getElementById('lost');
+let displayPlayed = document.getElementById('played');
 
 function flipCoin(x) {
     let pick = x;
+    let choice = document.getElementById('choice');
+    let flip =document.getElementById('flip');
+    let result = document.getElementById('result');
 
-    let flip = document.getElementById('flip');
-    let choice =document.getElementById('choice');
-    let game = document.getElementById('result');
-
+  
     let randNum = Math.ceil(Math.random() * 2);
+    let toss; 
 
-    let face;
-
-    if (randNum == 1) {
-        face = "Heads";
+    if (randNum === 1) {
+        toss = 'Heads';
     } else {
-        face = "Tails";
+        toss = 'Tails';
     }
 
+    let status;
+    if (pick === toss ) {
+        status = "Won";
+        gamesWon++; 
+    } else {
+        status = 'Lost';
+        gamesLost++;
+    }
+    
+    gamesPlayed++;
 
-
-    result.innerText = face;
+    // read-outs of the game results
     choice.innerText = pick;
+    flip.innerText = toss;
+    result.innerText = status; 
 
+    //updates game history
+    displayPlayed.innerText = gamesPlayed;
+    displayWon.innerText = gamesWon;
+    displayLost.innerText = gamesLost;
+
+    function resetScore() {
+        gamesPlayed = 0;
+        gamesWon = 0;
+        gamesLost = 0;
+
+        displayPlayed.innerText = gamesPlayed;
+        displayWon.innerText = gamesWon;
+        displayLost.innerText = gamesLost;
+    }
 
     console.log(randNum);
 }
